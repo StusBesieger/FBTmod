12c23cca92d7e6c384357c51680ffbebf7473429
stus <143853974+StusBesieger@users.noreply.github.com>
2023-09-04 22:34:09 +0900

    Add files via upload

--- Mod.cs
+++ Mod.cs
@@ -14,7 +14,7 @@ namespace FBTcore
 		public override void OnLoad()
 		{
 			mod = new GameObject("StusFBTcontroller");
-			SingleInstance<BlockSelector>.Instance.transform.parent = mod.transform;
+
 			UnityEngine.Object.DontDestroyOnLoad(mod);
 		}
 		public static void Log(string msg)
@@ -31,87 +31,47 @@ namespace FBTcore
 		}
 	}
 
-	//ブロックにスクリプトを追加
-	public class BlockSelector : SingleInstance<BlockSelector>
-	{
-		public Dictionary<string, Type> BlockDict = new Dictionary<string, Type>
-		{
-			//ブロックIDとスクリプト
-			//弾薬庫ブロック（未作成）
-			{"a3567e7a-50bd-41f4-87cc-1b93a3e93852-6", typeof(ExplosionScript)},
 
-		};
-		public override string Name
-		{
-			get
-			{
-				return "Stus FBT BlocksSelector";
-			}
-		}
-		//メソッド
-		public void Start()
-		{
-			Events.OnBlockInit += new Action<Block>(AddScript);
-		}
-		public void AddScript(Block block)
-		{
-			BlockBehaviour internalObject = block.BuildingBlock.InternalObject;
-			if (BlockDict.ContainsKey(internalObject.BlockID))
-			{
-				Type type = BlockDict[internalObject.BlockID];
-				try
-				{
-					//貼り付けている場合
-					if (internalObject.GetComponent(type) == null)
-					{
-						internalObject.gameObject.AddComponent(type);
-						Mod.Log("Added Script");
-					}
-				}
-				catch
-				{
-					Mod.Error("AddScript Error!");
-				}
-				return;
-			}
-		}
-	}
 
 	//ブロックhpが0になったら爆発するスクリプト
-	public class ExplosionScript : MonoBehaviour
+	public class ExplosionScript : BlockScript
 	{
 		private BlockBehaviour block;
+		public float radius = 5.0F;
+		public float power = 1000.0F;
 		//爆発ステータス
-		public float radius = 6.0F;
-		public float power = 10000.0F;
+
 		void Start()
 		{
+			Debug.Log("stus FBTmod : ");
 			block = base.GetComponent<BlockBehaviour>();
 		}
 		void FixedUpdate()
 		{
 
 			float hp;
-			if (block.Prefab.hasHealthBar)
+			if (this.gameObject.activeSelf)
 			{
 				hp = block.BlockHealth.health;
 				if (hp <= 0.01)
-					Explodey();
+					Explode();
+
 			}
 		}
-		private void Explodey()
+		//爆発
+		void Explode()
 		{
-				Vector3 explosionPos = transform.position;
-				Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
-				foreach (Collider hit in colliders)
-				{
-					Rigidbody rb = hit.GetComponent<Rigidbody>();
+			Debug.Log("stus FBTmod2 : ");
+			Vector3 explosionPos = transform.position;
+			Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+			foreach (Collider hit in colliders)
+			{
+				Rigidbody rb = hit.GetComponent<Rigidbody>();
 
-					if (rb != null)
-						rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
-
-				}
-			Destroy(this.gameObject);
+				if (rb != null)
+					rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+				this.gameObject.SetActive(false);
+			}
 		}
 	}
 }
